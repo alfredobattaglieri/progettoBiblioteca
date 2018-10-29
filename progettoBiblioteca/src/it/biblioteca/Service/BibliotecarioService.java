dcfdd20a427ea91c6a6dd7d440185cf81085e718
HP <HP@DESKTOP-C89F3N1.station>
2018-10-29 12:42:59 +0100

    BibliotecarioService

---
+++ progettoBiblioteca/src/it/biblioteca/Service/BibliotecarioService.java
@@ -0,0 +1,17 @@
+package it.biblioteca.Service;
+
+import java.util.ArrayList;
+
+import it.biblioteca.model.Bibliotecario;
+import it.biblioteca.model.Cliente;
+import it.biblioteca.model.Libro;
+import it.biblioteca.model.Regista;
+
+public interface BibliotecarioService {
+public Bibliotecario creaBibliotecario(long id);
+public Libro creaLibro(long id);
+public ArrayList<Regista> visualizzachiHaGiratoPiuFilm();
+public Cliente visualizzaRitardoNellaConsegna();
+public Cliente visualizzaChiLeggeDiPiuUnGenere();
+
+}
