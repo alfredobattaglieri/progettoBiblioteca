592a02df0c3ab98252941c92e9cb29a5d923728b
HP <HP@DESKTOP-C89F3N1.station>
2018-10-29 12:58:01 +0100

    ClienteService

--- progettoBiblioteca/src/it/biblioteca/Service/ClienteService.java
+++ progettoBiblioteca/src/it/biblioteca/Service/ClienteService.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import it.biblioteca.model.Cliente;
 import it.biblioteca.model.Libro;
 
-public interface UtenteService {
+public interface ClienteService {
 public Cliente creaCliente();
 public void affittaLibro(Libro libro);
 public ArrayList<Libro> visualizzaTuttiLibri();
---
+++ progettoBiblioteca/src/it/biblioteca/model/RegistaServiceImpl.java
@@ -0,0 +1,26 @@
+package it.biblioteca.model;
+
+import java.util.ArrayList;
+
+import it.biblioteca.Service.RegistaService;
+
+public class RegistaServiceImpl implements RegistaService {
+
+	@Override
+	public Regista creaRegista() {
+		
+	}
+
+	@Override
+	public ArrayList<Regista> visualizzaRegistaCheHaGiratoPiuFilm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Regista> visualizzaRegistaCheHaGiratoPiuFilmGenere() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
