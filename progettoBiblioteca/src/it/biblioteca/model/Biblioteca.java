242199a302f677d6bfea14142b8c2b4caec79e5e
feder <feder@LAPTOP-DL1JEVEN>
2018-10-30 09:55:31 +0100

    Biblioteca

--- progettoBiblioteca/src/it/biblioteca/model/Biblioteca.java
+++ progettoBiblioteca/src/it/biblioteca/model/Biblioteca.java
@@ -19,16 +19,16 @@ public class Biblioteca {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-<<<<<<< HEAD
+
 	public List<Libro> getLista_libro() {
 		return getLista_libro();
-=======
+    }
 	
 	
 	
 	public ArrayList<Romanzo> getLista_libro_romanzo() {
 		return lista_libro_romanzo;
->>>>>>> branch 'master' of https://github.com/alfredobattaglieri/progettoBiblioteca.git
+
 	}
 	public void setLista_libro_romanzo(ArrayList<Romanzo> lista_libro_romanzo) {
 		this.lista_libro_romanzo = lista_libro_romanzo;
@@ -59,14 +59,14 @@ public class Biblioteca {
 	public void setLista_libro_thriller(ArrayList<Libro> lista_libro_thriller) {
 		this.lista_libro_thriller = lista_libro_thriller;
 	}
-<<<<<<< HEAD
+
 	public Biblioteca(String nome, ArrayList<Libro> lista_libro_horror, ArrayList<Libro> lista_libro_thriller,
 			ArrayList<Bibliotecario> list_bibliotecario) {
-=======
-	public Biblioteca(String nome, ArrayList<Horror> lista_libro_horror, ArrayList<Thriller> lista_libro_thriller,
+		
+	}
+	public Biblioteca(String nome, ArrayList<Libro> lista_libro_horror, ArrayList<Libro> lista_libro_thriller,
 			ArrayList<Bibliotecario> list_bibliotecario, ArrayList<Romanzo> lista_libro_romanzo,
 			ArrayList<Storia> lista_libro_storia) {
->>>>>>> branch 'master' of https://github.com/alfredobattaglieri/progettoBiblioteca.git
 		super();
 		this.nome = nome;
 		this.lista_libro_horror = lista_libro_horror;
@@ -75,6 +75,7 @@ public class Biblioteca {
 		this.lista_libro_romanzo = lista_libro_romanzo;
 		this.lista_libro_storia = lista_libro_storia;
 	}
+
 	
 	
 	
